cfc6ef409287dcad650f94a1a4216e25b9657821
IsThisTheCrustyCrab-was-taken <kampe@uni-potsdam.de>
2025-02-23 18:57:13 +0100

    Initial Commit

---
+++ Hop On/Hop_OnApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hop_OnApp.swift
+//  Hop On
+//
+//  Created by Bennet Kampe on 23/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Hop_OnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
